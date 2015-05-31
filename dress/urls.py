c1a5b661d6b0a5bb6b00b12925c8b68667d1272b
JohnsonChen <johnson.chen@hopebaytech.com>
2015-05-31 16:36:18 +0800

    Add post dress url.

--- dress/urls.py
+++ dress/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from dress_pool.views import post_dress
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dress.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^dress/post/$', post_dress),
+    #url(r'^list/$', Hello_World),
 )
--- dress_pool/views.py
+++ dress_pool/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def post_dress(request):
+    return HttpResponse("Heelo World!!")
