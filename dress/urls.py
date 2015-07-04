4b02be8642e2c766778d9d23624e14d66ff4951b
ChinHui-Chen <dongogo@gmail.com>
2015-07-04 11:53:24 +0800

    Redirect admin.

--- dress/urls.py
+++ dress/urls.py
@@ -1,16 +1,21 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
-
 from dress_pool.views import post_dress
+from django.core.urlresolvers import reverse
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dress.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', RedirectView.as_view(url='/admin')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^dress/post/$', post_dress),
+    #url(r'^dress/post/$', post_dress),
     #url(r'^list/$', Hello_World),
 )
+
+
+
+
