33b43d79766b63e71880c715f3fe4748c869dfc3
ChinHui-Chen <dongogo@gmail.com>
2015-07-05 12:55:09 +0800

    Change to django suit.

--- dress_pool/admin.py
+++ dress_pool/admin.py
@@ -6,11 +6,17 @@ from dress_pool.models import Dress
 from dress_pool.models import RentRecord
 from dress_pool.models import Color
 
+class ModelAInline(admin.TabularInline):
+	model = RentRecord
+	extra = 0
+	ordering = ("-rentDate",)
+
 class DressAdmin(admin.ModelAdmin):
 	list_display = ('number','skirtType','dressType',)
 	search_fields = ['vendor']
 	readonly_fields = ('image_tag',)
-	
+	inlines = [ModelAInline]
+
 # Register your models here.
 admin.site.register(SkirtType)
 admin.site.register(DressType)
