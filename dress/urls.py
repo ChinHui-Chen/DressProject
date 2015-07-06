e86ff28ffd79c98d3eebcb61ba87ad7d4a19c0d5
ChinHui-Chen <dongogo@gmail.com>
2015-07-06 10:22:05 +0800

    Change default login page.

--- dress/urls.py
+++ dress/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'dress.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', RedirectView.as_view(url='/admin')),
+    url(r'^$', RedirectView.as_view(url='/admin/dress_pool/dress')),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^dress/post/$', post_dress),
